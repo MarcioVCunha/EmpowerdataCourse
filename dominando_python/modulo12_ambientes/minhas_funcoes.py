0618fe586f770a04de7ba815a9523bb7d640ac43
Marcio Cunha <marcioabcd11@gmail.com>
2023-07-13 14:34:44 -0300

    envs and modules

---
+++ dominando_python/modulo12_ambientes/minhas_funcoes.py
@@ -0,0 +1,8 @@
+def somar(*args):
+    return sum(args)
+
+def multiplicar(*args):
+    resultado = 1
+    for numero in args:
+        resultado *= numero
+    return resultado
---
+++ dominando_python/modulo12_ambientes/script.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import minhas_funcoes as mf
+from minhas_funcoes import somar
+
+print(mf.somar(10, 20, 30))
+print(somar(10, 20, 30))
+
+print(mf.multiplicar(10, 20, 30))
+
+print("Utilizando o ambiente virtual")
